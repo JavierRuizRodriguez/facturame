8f37e055322224fcccd6d4b78e8fb67207c1ee2c
Javier Ruiz Rodríguez <jruizrdgz@gmail.com>
2016-01-10 18:53:01 +0100

    refactor pojos

--- Facturame/src/pojo/Camion.java
+++ Facturame/src/pojo/Camion.java
@@ -8,48 +8,55 @@ public class Camion extends Vehiculo {
 	private double volumenCaja;
 	private boolean trampilla;
 	
-	public Camion(String tipo, String nBastidor, String matricula, String tCombustible, int nPasajeros, int potenciaCV,
+	public Camion(String nBastidor, String matricula, String combustible, int nPasajeros, int potenciaCV,
 			int potenciaKWh, int kmTotales, int peso, double largo, double ancho, String descripcion, double longCaja,
 			double anchoCaja, double pesoMaxCaja, double volumenCaja, boolean trampilla) {
-		super(tipo, nBastidor, matricula, tCombustible, nPasajeros, potenciaCV, potenciaKWh, kmTotales, peso, largo,
-				ancho, descripcion);
+		super(nBastidor, matricula, combustible, nPasajeros, potenciaCV, potenciaKWh, kmTotales, peso, largo, ancho,
+				descripcion);
 		this.longCaja = longCaja;
 		this.anchoCaja = anchoCaja;
 		this.pesoMaxCaja = pesoMaxCaja;
 		this.volumenCaja = volumenCaja;
-		this.trampilla = trampilla;
-	}
-	
-	public double getLongCaja() {
-		return longCaja;
-	}
-	public void setLongCaja(double longCaja) {
-		this.longCaja = longCaja;
-	}
-	public double getAnchoCaja() {
-		return anchoCaja;
-	}
-	public void setAnchoCaja(double anchoCaja) {
-		this.anchoCaja = anchoCaja;
-	}
-	public double getPesoMaxCaja() {
-		return pesoMaxCaja;
-	}
-	public void setPesoMaxCaja(double pesoMaxCaja) {
-		this.pesoMaxCaja = pesoMaxCaja;
-	}
-	public double getVolumenCaja() {
-		return volumenCaja;
-	}
-	public void setVolumenCaja(double volumenCaja) {
-		this.volumenCaja = volumenCaja;
-	}
-	public boolean isTrampilla() {
-		return trampilla;
-	}
-	public void setTrampilla(boolean trampilla) {
 		this.trampilla = trampilla;
 	}
 
-	
+	public double getLongCaja() {
+		return longCaja;
+	}
+
+	public void setLongCaja(double longCaja) {
+		this.longCaja = longCaja;
+	}
+
+	public double getAnchoCaja() {
+		return anchoCaja;
+	}
+
+	public void setAnchoCaja(double anchoCaja) {
+		this.anchoCaja = anchoCaja;
+	}
+
+	public double getPesoMaxCaja() {
+		return pesoMaxCaja;
+	}
+
+	public void setPesoMaxCaja(double pesoMaxCaja) {
+		this.pesoMaxCaja = pesoMaxCaja;
+	}
+
+	public double getVolumenCaja() {
+		return volumenCaja;
+	}
+
+	public void setVolumenCaja(double volumenCaja) {
+		this.volumenCaja = volumenCaja;
+	}
+
+	public boolean isTrampilla() {
+		return trampilla;
+	}
+
+	public void setTrampilla(boolean trampilla) {
+		this.trampilla = trampilla;
+	}	
 }
--- Facturame/src/pojo/Vehiculo.java
+++ Facturame/src/pojo/Vehiculo.java
@@ -2,10 +2,9 @@ package pojo;
 
 public abstract class Vehiculo {
 
-	private String tipo;
 	private String nBastidor;
 	private String matricula;
-	private String tCombustible;
+	private String combustible;
 	private int nPasajeros;
 	private int potenciaCV;
 	private int potenciaKWh;
@@ -15,117 +14,86 @@ public abstract class Vehiculo {
 	private double ancho;
 	private String descripcion;
 	//FALTA METERLE LOS PATRONES DE ENTRADA A LAS VARIABLES NBASTIDOR, MATRICULA, COMBUSTIBLE, ETC...
-
-	public Vehiculo(String tipo, String nBastidor, String matricula, String tCombustible, int nPasajeros,
-			int potenciaCV, int potenciaKWh, int kmTotales, int peso, double largo, double ancho, String descripcion) {
-		this.tipo = tipo;
+	public Vehiculo(String nBastidor, String matricula, String combustible, int nPasajeros, int potenciaCV,
+			int potenciaKWh, int kmTotales, int peso, double largo, double ancho, String descripcion) {
+		super();
 		this.nBastidor = nBastidor;
 		this.matricula = matricula;
-		this.tCombustible = tCombustible;
+		this.combustible = combustible;
 		this.nPasajeros = nPasajeros;
 		this.potenciaCV = potenciaCV;
 		this.potenciaKWh = potenciaKWh;
-		this.KmTotales = kmTotales;
+		KmTotales = kmTotales;
 		this.peso = peso;
 		this.largo = largo;
 		this.ancho = ancho;
 		this.descripcion = descripcion;
 	}
-
-	public String getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-
 	public String getnBastidor() {
 		return nBastidor;
 	}
-
 	public void setnBastidor(String nBastidor) {
 		this.nBastidor = nBastidor;
 	}
-
 	public String getMatricula() {
 		return matricula;
 	}
-
 	public void setMatricula(String matricula) {
 		this.matricula = matricula;
 	}
-
-	public String gettCombustible() {
-		return tCombustible;
+	public String getCombustible() {
+		return combustible;
 	}
-
-	public void settCombustible(String tCombustible) {
-		this.tCombustible = tCombustible;
+	public void setCombustible(String combustible) {
+		this.combustible = combustible;
 	}
-
 	public int getnPasajeros() {
 		return nPasajeros;
 	}
-
 	public void setnPasajeros(int nPasajeros) {
 		this.nPasajeros = nPasajeros;
 	}
-
 	public int getPotenciaCV() {
 		return potenciaCV;
 	}
-
 	public void setPotenciaCV(int potenciaCV) {
 		this.potenciaCV = potenciaCV;
 	}
-
 	public int getPotenciaKWh() {
 		return potenciaKWh;
 	}
-
 	public void setPotenciaKWh(int potenciaKWh) {
 		this.potenciaKWh = potenciaKWh;
 	}
-
 	public int getKmTotales() {
 		return KmTotales;
 	}
-
 	public void setKmTotales(int kmTotales) {
 		KmTotales = kmTotales;
 	}
-
 	public int getPeso() {
 		return peso;
 	}
-
 	public void setPeso(int peso) {
 		this.peso = peso;
 	}
-
 	public double getLargo() {
 		return largo;
 	}
-
 	public void setLargo(double largo) {
 		this.largo = largo;
 	}
-
 	public double getAncho() {
 		return ancho;
 	}
-
 	public void setAncho(double ancho) {
 		this.ancho = ancho;
 	}
-
 	public String getDescripcion() {
 		return descripcion;
 	}
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-
+	
 }
