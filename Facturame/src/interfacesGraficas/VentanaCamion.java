7ddc2f9831d0dafe685e4b4421f1082da2c92548
Jorge <darako.genesis@gmail.com>
2016-01-10 21:33:21 +0100

    VentanaEmpleado

--- Facturame/src/interfacesGraficas/VentanaCamion.java
+++ Facturame/src/interfacesGraficas/VentanaCamion.java
@@ -54,8 +54,9 @@ public class VentanaCamion extends JFrame {
 	 * Create the frame.
 	 */
 	public VentanaCamion() {
+		setTitle("Facturame --- Camion");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 554, 440);
+		setBounds(100, 100, 555, 440);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
